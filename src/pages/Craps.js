4859bfbaa7acf3c9d38ff9e9906a5872c83add8b
Dylan <60489717+DRoberts919@users.noreply.github.com>
2021-08-05 19:08:16 -0600

    back board setup

--- src/pages/Craps.js
+++ src/pages/Craps.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
-import crapsBoard from ""
+import "../craps.css";
 
 const Craps = () => {
   return (
     <div className="main">
       <h2 className="casino-font">Craps</h2>
       <Link to="/">Go Back</Link>
+      <div className="crapsBoard"></div>
     </div>
   );
 };
