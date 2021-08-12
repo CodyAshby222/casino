ab22c911518b4179a4a5c8ae4520b83ca4759c6c
John <jashby@student.neumont.edu>
2021-08-11 20:11:09 -0600

    chip component added

---
+++ src/components/Chips.jsx
@@ -0,0 +1,20 @@
+const Chips = ({ setChipSelected }) => {
+  const allChips = [1, 5, 10, 20, 50, 100, 500, 1000, 5000];
+
+  return (
+    <div className="row">
+      {allChips.map((chip, index) => {
+        return (
+          <img
+            className="chip"
+            onClick={() => setChipSelected(chip)}
+            src={`/Overall UI/Chip${chip}.png`}
+            alt={`Chip_${chip}`}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Chips;
--- src/pages/Craps.js
+++ src/pages/Craps.js
@@ -1,12 +1,57 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Chips from "../components/Chips";
 import "../craps.css";
 
 const Craps = () => {
+  const [chip, setChip] = useState();
+
+  const chipHandler = (chipValue) => {
+    setChip(chipValue);
+  };
+
   return (
     <div className="main">
       <h2 className="casino-font">Craps</h2>
       <Link to="/">Go Back</Link>
-      <div className="crapsBoard"></div>
+      <div className="crapsBoard">
+        {/* Top Section */}
+        <div className="row">
+          <div className="craps-btn"></div>
+          <div className="craps-btn"></div>
+          <div className="craps-btn"></div>
+          <div className="craps-btn"></div>
+          <div className="craps-btn"></div>
+          <div className="craps-btn"></div>
+          <div style={{ marginLeft: 32 }}>
+            <div style={{ marginTop: 43 }} className="row">
+              <div className="hardways-btn"></div>
+              <div className="hardways-btn"></div>
+            </div>
+            <div className="row">
+              <div className="hardways-btn"></div>
+              <div className="hardways-btn"></div>
+            </div>
+          </div>
+        </div>
+        {/* Bottom Section */}
+        <div className="row">
+          <div className="field-btn"></div>
+          <div style={{ marginTop: 52, marginLeft: 31 }}>
+            <div className="one-long-roll-btn"></div>
+            <div className="row">
+              <div className="one-roll-btn"></div>
+              <div className="one-roll-btn"></div>
+            </div>
+            <div className="row">
+              <div className="one-roll-btn"></div>
+              <div className="one-roll-btn"></div>
+            </div>
+            <div className="one-long-roll-btn"></div>
+          </div>
+        </div>
+        <Chips setChipSelected={chipHandler} />
+      </div>
     </div>
   );
 };
