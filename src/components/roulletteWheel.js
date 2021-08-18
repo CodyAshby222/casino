362aed62ce2fbe1972a6242f04a145dd1a566235
Dylan <60489717+DRoberts919@users.noreply.github.com>
2021-08-17 18:11:09 -0600

    roullette wheel

---
+++ src/components/roulletteWheel.js
@@ -0,0 +1,152 @@
+const rouletterNumbers = [
+  {
+    num: 0,
+    color: "green",
+  },
+  {
+    num: 1,
+    color: "red",
+  },
+  {
+    num: 2,
+    color: "black",
+  },
+  {
+    num: 3,
+    color: "red",
+  },
+  {
+    num: 4,
+    color: "red",
+  },
+  {
+    num: 5,
+    color: "red",
+  },
+  {
+    num: 6,
+    color: "red",
+  },
+  {
+    num: 7,
+    color: "red",
+  },
+  {
+    num: 8,
+    color: "red",
+  },
+  {
+    num: 9,
+    color: "red",
+  },
+  {
+    num: 10,
+    color: "red",
+  },
+  {
+    num: 11,
+    color: "red",
+  },
+  {
+    num: 12,
+    color: "red",
+  },
+  {
+    num: 13,
+    color: "red",
+  },
+  {
+    num: 14,
+    color: "red",
+  },
+  {
+    num: 15,
+    color: "red",
+  },
+  {
+    num: 16,
+    color: "red",
+  },
+  {
+    num: 17,
+    color: "red",
+  },
+  {
+    num: 18,
+    color: "red",
+  },
+  {
+    num: 19,
+    color: "red",
+  },
+  {
+    num: 20,
+    color: "red",
+  },
+  {
+    num: 21,
+    color: "red",
+  },
+  {
+    num: 22,
+    color: "red",
+  },
+  {
+    num: 23,
+    color: "red",
+  },
+  {
+    num: 24,
+    color: "red",
+  },
+  {
+    num: 25,
+    color: "red",
+  },
+  {
+    num: 26,
+    color: "red",
+  },
+  {
+    num: 27,
+    color: "red",
+  },
+  {
+    num: 28,
+    color: "red",
+  },
+  {
+    num: 29,
+    color: "red",
+  },
+  {
+    num: 30,
+    color: "red",
+  },
+  {
+    num: 31,
+    color: "red",
+  },
+  {
+    num: 32,
+    color: "red",
+  },
+  {
+    num: 33,
+    color: "red",
+  },
+  {
+    num: 34,
+    color: "red",
+  },
+  {
+    num: 35,
+    color: "red",
+  },
+  {
+    num: 36,
+    color: "red",
+  },
+];
+
+export { rouletterNumbers };
