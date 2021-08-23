89c10d66820806cc0c61487baceafb095caa0470
John <jashby@student.neumont.edu>
2021-08-23 11:19:28 -0600

    craps update

--- src/pages/Craps.js
+++ src/pages/Craps.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useContext, useState } from "react";
+import { BankContext } from "../App";
 import Button from "../components/Button";
 import Chips from "../components/Chips";
 import Rules from "../components/Rules";
@@ -10,13 +11,18 @@ const Craps = () => {
   const [chipLocation, setChipLocation] = useState();
   const [dieOne, setDieOne] = useState();
   const [dieTwo, setDieTwo] = useState();
-
-  useEffect(() => {}, []);
-
+  const [bank, setBank] = useContext(BankContext);
   const crapsBtns = new Array(6).fill("craps-btn");
 
   const chipHandler = (chipValue) => {
     setChip(chipValue);
+  };
+
+  const handleChipClick = (location) => {
+    if (!chipLocation) {
+      setBank((prev) => prev - chip);
+    }
+    setChipLocation(location);
   };
 
   const handleChipLocation = (currentLocation, chipClass) => {
@@ -34,14 +40,12 @@ const Craps = () => {
   };
 
   const rollDice = () => {
-    //Dice Values
     let dieOneNum = Math.floor(Math.random() * 6) + 1;
     let dieTwoNum = Math.floor(Math.random() * 6) + 1;
     let dieTotal = dieOneNum + dieTwoNum;
     setDieOne(dieOneNum);
     setDieTwo(dieTwoNum);
 
-    //Location and Logic
     const hardwaysArr = ["twos", "threes", "fours", "fives"];
     const oneRollArr = ["anyCraps", 7, 3, 11, 2, 12];
 
@@ -50,32 +54,108 @@ const Craps = () => {
     } else if (chipLocation === "craps-field") {
       fieldLogic(dieTotal);
     } else if (hardwaysArr.includes(chipLocation)) {
-      hardwaysLogic(dieOneNum, dieTwoNum, dieTotal);
+      hardwaysLogic(dieOneNum, dieTotal);
     } else if (oneRollArr.includes(chipLocation)) {
-      oneRollLogic(dieOneNum, dieTwoNum, dieTotal);
-    } else {
-      //Chip hasn't been placed
+      oneRollLogic(dieTotal);
     }
   };
 
   const rowLogic = (dieTotal) => {
-    console.log("IM ON ROW");
-    //1:2?
+    const potentialWins = [4, 5, 6, 8, 9, 10];
+    if (dieTotal !== 7) {
+      potentialWins.forEach((value, i) => {
+        if (chipLocation === `craps-btn${i}` && dieTotal === value) {
+          if (i === 0 || i === 5)
+            setBank((prev) => Math.floor(prev + chip + chip * 1.8));
+          if (i === 1 || i === 4)
+            setBank((prev) => Math.floor(prev + chip + chip * 1.4));
+          if (i === 2 || i === 3)
+            setBank((prev) => Math.floor(prev + chip + chip * 1.1666));
+
+          setChipLocation();
+        }
+      });
+    } else {
+      setChipLocation();
+    }
   };
 
   const fieldLogic = (dieTotal) => {
-    console.log("IM ON FIELD");
-    // 1:1 on 3,4,9,10,11... 2:1 on 2... 3:1 on 12
+    const potentialWins = [2, 3, 4, 9, 10, 11, 12];
+    const potentialLosses = [1, 5, 6, 7, 8];
+    potentialLosses.forEach((value, i) => {
+      if (dieTotal === value) {
+        setChipLocation();
+      }
+    });
+    potentialWins.forEach((value, i) => {
+      if (dieTotal === value && value === 12) {
+        setBank((prev) => prev + chip + chip * 3);
+        setChipLocation();
+      }
+      if (dieTotal === value && value === 2) {
+        setBank((prev) => prev + chip + chip * 2);
+        setChipLocation();
+      }
+      if (dieTotal === value && value !== 12 && value !== 2) {
+        setBank((prev) => prev + chip + chip);
+        setChipLocation();
+      }
+    });
   };
 
-  const hardwaysLogic = (dieOne, dieTwo, dieTotal) => {
-    console.log("IM ON HARDWAYS");
-    //Keep until same num or craps hit?
+  const hardwaysLogic = (dieOne, dieTotal) => {
+    const potentialLocations = ["twos", "fives", "threes", "fours"];
+    const potentialWins = [4, 10, 6, 8];
+
+    potentialLocations.forEach((location, i) => {
+      if (
+        location === chipLocation &&
+        potentialWins[i] === dieTotal &&
+        potentialWins[i] / 2 === dieOne
+      ) {
+        if (i === 0 || i === 1) {
+          setBank((prev) => prev + chip + chip * 7);
+          setChipLocation();
+        } else {
+          setBank((prev) => prev + chip + chip * 9);
+          setChipLocation();
+        }
+      } else if (
+        (location === chipLocation &&
+          potentialWins[i] === dieTotal &&
+          potentialWins[i] / 2 !== dieOne) ||
+        dieTotal === 7
+      ) {
+        setChipLocation();
+      }
+    });
   };
 
-  const oneRollLogic = (dieOne, dieTwo, dieTotal) => {
-    console.log("I ON ONE BEt");
-    //Roll and Reset Roll
+  const oneRollLogic = (dieTotal) => {
+    console.log(chipLocation, dieTotal);
+    if (chipLocation === 7 && dieTotal === 7) {
+      setBank((prev) => prev + chip + chip * 4);
+    }
+    if (
+      chipLocation === "anyCraps" &&
+      (dieTotal === 2 || dieTotal === 3 || dieTotal === 12)
+    ) {
+      setBank((prev) => prev + chip + chip * 7);
+    }
+    if (
+      (chipLocation === 3 && dieTotal === 3) ||
+      (chipLocation === 11 && dieTotal === 11)
+    ) {
+      setBank((prev) => prev + chip + chip * 15);
+    }
+    if (
+      (chipLocation === 2 && dieTotal === 2) ||
+      (chipLocation === 12 && dieTotal === 12)
+    ) {
+      setBank((prev) => prev + chip + chip * 30);
+    }
+    setChipLocation();
   };
 
   return (
@@ -95,7 +175,7 @@ const Craps = () => {
               return (
                 <div
                   key={`${btn}_${index}`}
-                  onClick={() => setChipLocation(`${btn}${index}`)}
+                  onClick={() => handleChipClick(`${btn}${index}`)}
                   className={`${btn} craps-center`}
                 >
                   {handleChipLocation(`${btn}${index}`, "craps-middle-chip")}
@@ -105,13 +185,13 @@ const Craps = () => {
             <div style={{ marginLeft: 32 }}>
               <div style={{ marginTop: 43 }} className="row">
                 <div
-                  onClick={() => setChipLocation("twos")}
+                  onClick={() => handleChipClick("twos")}
                   className="hardways-btn craps-center"
                 >
                   {handleChipLocation("twos", "craps-chip")}
                 </div>
                 <div
-                  onClick={() => setChipLocation("fives")}
+                  onClick={() => handleChipClick("fives")}
                   className="hardways-btn craps-center"
                 >
                   {handleChipLocation("fives", "craps-chip")}
@@ -119,13 +199,13 @@ const Craps = () => {
               </div>
               <div className="row">
                 <div
-                  onClick={() => setChipLocation("threes")}
+                  onClick={() => handleChipClick("threes")}
                   className="hardways-btn craps-center"
                 >
                   {handleChipLocation("threes", "craps-chip")}
                 </div>
                 <div
-                  onClick={() => setChipLocation("fours")}
+                  onClick={() => handleChipClick("fours")}
                   className="hardways-btn craps-center"
                 >
                   {handleChipLocation("fours", "craps-chip")}
@@ -154,7 +234,7 @@ const Craps = () => {
                 ) : null}
               </div>
               <div
-                onClick={() => setChipLocation("craps-field")}
+                onClick={() => handleChipClick("craps-field")}
                 className="field-btn craps-center"
               >
                 {handleChipLocation("craps-field", "craps-middle-chip")}
@@ -162,20 +242,20 @@ const Craps = () => {
             </div>
             <div style={{ marginTop: 52, marginLeft: 31 }}>
               <div
-                onClick={() => setChipLocation(7)}
+                onClick={() => handleChipClick(7)}
                 className="one-long-roll-btn craps-center"
               >
                 {handleChipLocation(7, "craps-chip")}
               </div>
               <div className="row">
                 <div
-                  onClick={() => setChipLocation(3)}
+                  onClick={() => handleChipClick(3)}
                   className="one-roll-btn craps-center"
                 >
                   {handleChipLocation(3, "craps-chip")}
                 </div>
                 <div
-                  onClick={() => setChipLocation(11)}
+                  onClick={() => handleChipClick(11)}
                   className="one-roll-btn craps-center"
                 >
                   {handleChipLocation(11, "craps-chip")}
@@ -183,20 +263,20 @@ const Craps = () => {
               </div>
               <div className="row">
                 <div
-                  onClick={() => setChipLocation(2)}
+                  onClick={() => handleChipClick(2)}
                   className="one-roll-btn craps-center"
                 >
                   {handleChipLocation(2, "craps-chip")}
                 </div>
                 <div
-                  onClick={() => setChipLocation(12)}
+                  onClick={() => handleChipClick(12)}
                   className="one-roll-btn craps-center"
                 >
                   {handleChipLocation(12, "craps-chip")}
                 </div>
               </div>
               <div
-                onClick={() => setChipLocation("anyCraps")}
+                onClick={() => handleChipClick("anyCraps")}
                 className="one-long-roll-btn craps-center"
               >
                 {handleChipLocation("anyCraps", "craps-chip")}
@@ -204,6 +284,13 @@ const Craps = () => {
             </div>
           </div>
           <Chips setChipSelected={chipHandler} />
+          <div
+            style={{ textAlign: "right", marginTop: -100 }}
+            className="white-text casino-font"
+          >
+            <h2>CHIPS:</h2>
+            <h2>${bank}</h2>
+          </div>
           <br />
           <div className="row-space-between">
             <Button title={"BACK"} linkTo={"/"} />
